e863bab1469fb96b39ad5e4edeff999cb9247f72
Nikhil-Devang <devangreshu@gmail.com>
2025-03-09 11:14:57 +0530

    Array.py

--- paliandrom.py
+++ paliandrom.py
@@ -27,3 +27,18 @@ while i<=j:
         break
     i+=1
     j-=1
+
+#Check if there exist a triple with sum = target
+arr=[1,2,3,4,5,6,7]
+i=0
+j=1
+k=2
+target=10
+for i in range(0,len(arr)):
+    for j in range(i+1,len(arr)):
+        for k in range(j+1,len(arr)):
+            x=arr[i]
+            y=arr[j]
+            z=arr[k]
+            if x+y+z==target:
+                print(arr[i],arr[j],arr[k])
